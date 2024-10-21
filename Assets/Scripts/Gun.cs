d1c5d8c9412a2b400f1590af0bda2b933dd5dd69
Nikolas-Brouwer <nikolas.brouwer@okstate.edu>
2024-10-20 23:43:22 -0500

    Music
    More fills, synced reload with beat

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 public class Gun : MonoBehaviour
 {
@@ -23,22 +24,22 @@ public class Gun : MonoBehaviour
             if(Input.GetMouseButtonDown(0) && !PauseMenu.isPaused){
                 switch(TimeKeeper.instance.chord){
                     case(1):
-                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 1f, 0);
+                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 0.7f, 0);
                     break;
                     case(4):
-                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 1f, 1);
+                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 0.7f, 1);
                     break;
                     case(5):
-                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 1f, 2);
+                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 0.7f, 2);
                     break;
                     case(3):
-                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 1f, 3);
+                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 0.7f, 3);
                     break;
                     case(7):
-                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 1f, 4);
+                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 0.7f, 4);
                     break;
                     case(6):
-                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 1f, 5);
+                    SoundFX.instance.PlaySoundFX(emptyChamber, firePoint, 0.7f, 5);
                     break;
                 }
             }
@@ -66,52 +67,58 @@ public class Gun : MonoBehaviour
         projectile.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
         switch(TimeKeeper.instance.chord){
             case(1):
-            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 1f, 0);
+            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 0.7f, 0);
             break;
             case(4):
-            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 1f, 1);
+            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 0.7f, 1);
             break;
             case(5):
-            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 1f, 2);
+            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 0.7f, 2);
             break;
             case(3):
-            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 1f, 3);
+            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 0.7f, 3);
             break;
             case(7):
-            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 1f, 4);
+            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 0.7f, 4);
             break;
             case(6):
-            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 1f, 5);
+            SoundFX.instance.PlaySoundFX(gunshots, firePoint, 0.7f, 5);
             break;
         }
     }
     private IEnumerator Reload(){
         isReloading = true;
-        switch(TimeKeeper.instance.chord){
-                    case(1):
-                    SoundFX.instance.PlaySoundFX(reloading, firePoint, 1f, 0, true);
-                    break;
-                    case(4):
-                    SoundFX.instance.PlaySoundFX(reloading, firePoint, 1f, 1, true);
-                    break;
-                    case(5):
-                    SoundFX.instance.PlaySoundFX(reloading, firePoint, 1f, 2, true);
-                    break;
-                    case(3):
-                    SoundFX.instance.PlaySoundFX(reloading, firePoint, 1f, 3, true);
-                    break;
-                    case(7):
-                    SoundFX.instance.PlaySoundFX(reloading, firePoint, 1f, 4, true);
-                    break;
-                    case(6):
-                    SoundFX.instance.PlaySoundFX(reloading, firePoint, 1f, 5, true);
-                    break;
-                }
+        ReloadSound();
         yield return new WaitForSeconds(reloadTime);
         curAmmo = maxAmmo;
         foreach (SpriteRenderer sr in ammoSprite){
             sr.enabled = true;
         }
         isReloading = false;
+    }
+    private async void ReloadSound(){
+        while(!TimeKeeper.instance.startBeat){
+            await Task.Yield();
+        }
+        switch(TimeKeeper.instance.chord){
+            case(1):
+            SoundFX.instance.PlaySoundFX(reloading, firePoint, 0.7f, 0, true);
+            break;
+            case(4):
+            SoundFX.instance.PlaySoundFX(reloading, firePoint, 0.7f, 1, true);
+            break;
+            case(5):
+            SoundFX.instance.PlaySoundFX(reloading, firePoint, 0.7f, 2, true);
+            break;
+            case(3):
+            SoundFX.instance.PlaySoundFX(reloading, firePoint, 0.7f, 3, true);
+            break;
+            case(7):
+            SoundFX.instance.PlaySoundFX(reloading, firePoint, 0.7f, 4, true);
+            break;
+            case(6):
+            SoundFX.instance.PlaySoundFX(reloading, firePoint, 0.7f, 5, true);
+            break;
+        }
     }
 }
